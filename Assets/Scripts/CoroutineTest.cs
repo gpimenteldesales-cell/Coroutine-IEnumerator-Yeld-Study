7d5b0fef9a0ecff27f0a43552ed5163d4d770204
GUIMê <gpimenteldesales@gmail.com>
2026-07-09 16:15:32 -0300

    CoroutineTestTimeToFlip

--- Assets/Scripts/CoroutineTest.cs
+++ Assets/Scripts/CoroutineTest.cs
@@ -1,40 +1,62 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
-public class Coroutine : MonoBehaviour
+public class CoroutineTest : MonoBehaviour
 {
+    /// 
+    /// 
+    ///
+    public InputAction keysControl;
     public InputAction clickControl;
     private SpriteRenderer spritePlayer;
     public GameObject textObject;
     public TextMeshProUGUI textTexto;
+    public Coroutine coroutineChange;
 
     public int scoreTime = 0;
+    ///
+    ///
+    ///
 
     void OnEnable()
     {
         clickControl.Enable();
         clickControl.performed += ClickCountTime;
+        keysControl.Enable();
+        keysControl.performed += BreakCoroutine;
     }
     void OnDisable()
     {
         clickControl.Disable();
+        keysControl.Disable();
     }
-
     void Start()
     {
         spritePlayer = GetComponent<SpriteRenderer>();
+        coroutineChange = StartCoroutine(Changes());
     }
 
     void Update()
     {
 
     }
-
+    IEnumerator Changes()
+    {
+        spritePlayer.color = Color.green;
+        yield return new WaitForSeconds(3f);
+        spritePlayer.flipX = true;
+    }
     void ClickCountTime(InputAction.CallbackContext context)
     {
         scoreTime++;
         textTexto.text = "Clicks:" + scoreTime.ToString();
         spritePlayer.color = Color.red;
+    }
+    void BreakCoroutine(InputAction.CallbackContext context)
+    {
+        StopCoroutine(coroutineChange);
+        Debug.Log("Parou! :D");
     }
 }
