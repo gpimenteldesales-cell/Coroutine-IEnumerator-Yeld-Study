c8afebbf25e82991bca8e158784d0a0a9e2c2772
GUIMê <gpimenteldesales@gmail.com>
2026-07-09 14:57:55 -0300

    ScriptCountClicks

---
+++ Assets/Scripts/Coroutine.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Coroutine : MonoBehaviour
+{
+    public InputAction clickControl;
+    private SpriteRenderer spritePlayer;
+    public GameObject textObject;
+    public TextMeshProUGUI textTexto;
+
+    public int scoreTime = 0;
+
+    void OnEnable()
+    {
+        clickControl.Enable();
+        clickControl.performed += ClickCountTime;
+    }
+    void OnDisable()
+    {
+        clickControl.Disable();
+    }
+
+    void Start()
+    {
+        spritePlayer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void ClickCountTime(InputAction.CallbackContext context)
+    {
+        scoreTime++;
+        textTexto.text = "Clicks:" + scoreTime.ToString();
+    }
+}
